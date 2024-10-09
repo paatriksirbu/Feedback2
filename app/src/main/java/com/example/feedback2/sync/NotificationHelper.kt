4b3897d02bf086f19877a96c11425f536bfb0a29
paatriksirbu <psirb@myuax.com>
2024-10-09 23:54:53 +0200

    NotificationHelper para gestionar notificaciones de sincronizacion con manejo de permisos. Requerido en Android 13+

---
+++ app/src/main/java/com/example/feedback2/sync/NotificationHelper.kt
@@ -0,0 +1,52 @@
+package com.example.feedback2.sync
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.feedback2.R
+
+object NotificationHelper {
+    private const val CHANNEL_ID = "sync_channel"
+    private const val CHANNEL_NAME = "sync Notifications"
+    private const val CHANNEL_DESCRIPTION = "Notificaciones relacionadas con la sincronizacion de datos"
+
+    fun showSyncCompleteNotification(context: Context){
+
+        createNotificationChannel(context)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED){
+                return
+            }
+        }
+        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.syncIcon)
+            .setContentTitle("Sincronización completada")
+            .setContentText("Se han sincronizado los datos correctamente")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        with(NotificationManagerCompat.from(context)){
+            notify(1, notification)
+        }
+
+    }
+
+    private fun createNotificationChannel(context: Context){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance).apply{
+                description = CHANNEL_DESCRIPTION
+            }
+
+            val notificationManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+
+        }
+    }
+}
--- app/src/main/java/com/example/feedback2/sync/SyncDataWorker.kt
+++ app/src/main/java/com/example/feedback2/sync/SyncDataWorker.kt
@@ -6,6 +6,7 @@ import androidx.work.WorkerParameters
 import com.example.feedback2.repository.NovelRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import com.example.feedback2.userInterface.NotificationHelper
 
 class SyncDataWorker(context: Context, params: WorkerParameters) : CoroutineWorker(context, params) {
 
@@ -16,8 +17,13 @@ class SyncDataWorker(context: Context, params: WorkerParameters) : CoroutineWork
             withContext(Dispatchers.IO){
                 syncDataFromFirebase()
             }
+
+            //Mostramos una notificacion al usuario para indicar que se ha completado la sincronizacion.
+            NotificationHelper.showSyncCompleteNotification(applicationContext)
             Result.success()
         } catch (e: Exception){
+            //En el caso de que algo falle, devolvemos un resultado fallido.
+            e.printStackTrace()
             Result.failure()
         }
     }

cf92d88363f5a8e472eb65b1d89ca93935833ac4
paatriksirbu <psirb@myuax.com>
2024-10-11 10:20:59 +0200

    Ajustes en los metodos Insert.

--- app/src/main/java/com/example/feedback2/data/NovelDAO.kt
+++ app/src/main/java/com/example/feedback2/data/NovelDAO.kt
@@ -3,16 +3,17 @@ package com.example.feedback2.data
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 
 @Dao
 interface NovelDAO {
 
-   @Insert
+   @Insert(onConflict = OnConflictStrategy.REPLACE)
    fun insert(novel: Novel)
 
-   @Insert
+   @Insert(onConflict = OnConflictStrategy.REPLACE)
    fun insertAll(novels: List<Novel>)
 
    @Update
